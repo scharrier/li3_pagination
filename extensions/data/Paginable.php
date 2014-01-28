4c71e4dc14dac430402f8852df60b84622166346
scharrier <scharrier@gmail.com>
2014-01-28 01:03:22 +0100

    First commit. Helper needs a lot of refactoring.

---
+++ extensions/data/Paginable.php
@@ -0,0 +1,49 @@
+<?php
+namespace li3_pagination\extensions\data ;
+
+use lithium\action\Request;
+use li3_pagination\extensions\data\Set;
+
+/**
+ * Prend en charge la pagination depuis une requête HTTP.
+ */
+trait Paginable {
+
+	/**
+	 * Exécute les deux requêtes nécessaires à la pagination : le count et la récupération des résultats
+	 * de la page courant.
+	 *
+	 * @param  Request $request Requête Lithium
+	 * @param  array  $query    Paramètres complémentaires de requêtage
+	 * @return PaginableSet     Set de données pris en charge par le helper de pagination
+	 */
+	public static function paginate(Request $request, array $query = []) {
+		$query += ['limit' => 20] ;
+
+		$page = static::_page($request) ;
+
+		$total = static::count($query) ;
+		$records = static::all([
+			'page' => $page
+		] + $query) ;
+
+		$return = new Set($records) ;
+		$return->meta(['page' => $page, 'total' => $total, 'limit' => $query['limit']]) ;
+
+		return $return ;
+	}
+
+	/**
+	 * Récupère le numéro de la page courante
+	 *
+	 * @param  Request $request Requête Lithium
+	 * @return int              Page courante. 1 par défaut.
+	 */
+	protected static function _page(Request $request) {
+		if (isset($request->query['page']) && is_numeric($request->query['page']) && $request->query['page'] > 0) {
+			return $request->query['page'] ;
+		}
+
+		return 1 ;
+	}
+}
---
+++ extensions/data/Set.php
@@ -0,0 +1,83 @@
+<?php
+namespace li3_pagination\extensions\data ;
+
+use lithium\data\collection\DocumentSet;
+
+/**
+ * Set de données embarquant les méta de pagination. Evite d'avoir à transmettre
+ * des trucs à la main. Un peu hacky sur les bords, mais ça va être bien pratique.
+ */
+class Set implements \IteratorAggregate, \Countable {
+
+	/**
+	 * Meta données de pagination.
+	 *
+	 * @var array
+	 */
+	protected $_meta = array() ;
+
+	/**
+	 * Constructeur.
+	 * @param DocumentSet $set  Set de données
+	 * @param array       $meta (optionnel) Méta données
+	 */
+	public function __construct(DocumentSet $set, array $meta = []) {
+		$this->_set = $set ;
+	}
+
+	/**
+	 * Permet d'accéder au vrai set.
+	 *
+	 * @return \lithium\data\collection\DocumentSet Set de document
+	 */
+	public function documents() {
+		return $this->_set ;
+	}
+
+	/**
+	 * Getter/setter de méta données.
+	 *
+	 * @param  array $meta Setter : tableau de méta
+	 * @return array       Getter : tableau de méta
+	 */
+	public function meta($meta = null) {
+		if (!isset($meta)) {
+			return $this->_meta ;
+		}
+
+		$this->_meta = $meta + $this->_meta ;
+	}
+
+	/**
+	 * Accessor magique. Permet d'accéder aux méta donnée en direct.
+	 *
+	 * Exemple :
+	 * {{{
+	 * $set->limit ; // Accède à $this->_meta['limit']
+	 * }}}
+	 *
+	 * @param  string $key Clé à laquelle on veut accéder
+	 * @return mixed       Valeur
+	 */
+	public function __get($key) {
+		return isset($this->_meta[$key]) ? $this->_meta[$key] : null ;
+	}
+
+	/**
+	 * Implémentation d'IteratorAggregate. Retourne le vrai itérator.
+	 *
+	 * @return Iterator Le set de documents
+	 */
+	public function getIterator() {
+		return $this->_set ;
+	}
+
+	/**
+	 * Implémentation de Countable.
+	 *
+	 * @return int Nombre de documents dans le set
+	 */
+	public function count() {
+		return $this->_set->count() ;
+	}
+}
